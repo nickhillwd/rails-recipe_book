0bfc8aa56c462383778a58c2b6628276da3318c3
nickhillwd <nickhillwd@gmail.com>
2015-12-21 09:37:46 +0000

    added recipe id column to Ingretient table rake db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151218150840) do
+ActiveRecord::Schema.define(version: 20151221093600) do
 
   create_table "Users", force: :cascade do |t|
     t.string   "user_name"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20151218150840) do
     t.string   "ingredient_name"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.integer  "recipe_id"
   end
 
   create_table "recipes", force: :cascade do |t|
