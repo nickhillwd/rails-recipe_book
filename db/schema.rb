b4632822e8af8be14364fda772d70ae17ef2faa5
nickhillwd <nickhillwd@gmail.com>
2015-12-21 09:40:47 +0000

    added recipe id to user column rake db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151221093600) do
+ActiveRecord::Schema.define(version: 20151221093913) do
 
   create_table "Users", force: :cascade do |t|
     t.string   "user_name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20151221093600) do
     t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "recipe_id"
   end
 
   create_table "catagories", force: :cascade do |t|
