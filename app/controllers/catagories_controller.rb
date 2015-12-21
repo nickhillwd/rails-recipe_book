8431e71f49c3042d2c8201744e71e3a765f4f488
nickhillwd <nickhillwd@gmail.com>
2015-12-21 16:04:54 +0000

    catagory title showing on catagory show page

--- app/controllers/catagories_controller.rb
+++ app/controllers/catagories_controller.rb
@@ -1,7 +1,11 @@
 class CatagoriesController < ApplicationController
 
   def show
+    @catagory = Catagory.find(params[:id])
 
+    id = params[:id]
+
+    @recipes = Recipe.find_by_sql("SELECT * FROM recipes WHERE catagory_id = #{id} ORDER BY recipe_name desc")
   end
 
 end
