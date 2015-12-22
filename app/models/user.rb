69995abfe4479876d7fc2b0a908b0fde0f1bfa34
nickhillwd <nickhillwd@gmail.com>
2015-12-22 10:39:40 +0000

    user input validation tested validation for presence and length of user name and email 5 tests passed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
+  validates(:user_name, presence: true, length: {maximum: 50})
+  validates(:user_email, presence: true, length: {maximum: 255})
   has_many :recipes
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @user = User.new(user_name: "Example User", user_email: "user@example.com")
+  end
+
+  test "should be vaild" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.user_name = "     "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.user_email = "      "
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long less than 50 characters" do
+    @user.user_name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long less than 255 characters for dB" do
+    @user.user_email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
 end
