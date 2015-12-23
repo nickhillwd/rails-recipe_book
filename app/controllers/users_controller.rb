b5526c0079318b90ad3b8e4398acc0aacada700c
nickhillwd <nickhillwd@gmail.com>
2015-12-23 09:56:01 +0000

    user sign up form added and persisting to User dB

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,22 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     #debugger
+    
   end
 
   def new
+    @user = User.new
   end
+
+  def create
+    User.create(user_params)
+    redirect_to(user_path[params :id])
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:user_name, :user_email, :password, :password_confirmation)
+    end
+
 end
