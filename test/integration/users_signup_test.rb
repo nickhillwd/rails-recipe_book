1eb9765d8833a24fc23db3b64949b9ba73dc9357
nickhillwd <nickhillwd@gmail.com>
2015-12-23 15:30:24 +0000

    IsLoggedIn method test passed

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,17 +2,29 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
  
-  # test "invalid sign up information" do
+  test "invalid sign up information" do
 
-  #   get new_user_path
-  #   assert_no_difference 'User.count' do
-  #     post users_path, user: { name: "",
-  #                     email: "user@invalid",
-  #                     password: "foo",
-  #                     password_confirmation: "bar"}
-  #   end
+    get new_user_path
+    assert_no_difference 'User.count' do
+      post users_path, user: { name: "",
+                      email: "user@invalid",
+                      password: "foo",
+                      password_confirmation: "bar"}
+    end
 
-  #   #assert_template 'users/new'
-  # end
+    assert_template 'users/new'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { user_name: "Example User",
+                                            user_email: "user@example.com",
+                                            password: "Password1",
+                                            password_confirmation: "Password1" }
+    end
+    assert_template 'users/show'
+    assert is_logged_in?
+  end
 
 end
