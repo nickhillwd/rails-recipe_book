1d7907b778d7c9c8dbf9a3fa4474a52c503db3f9
nickhillwd <nickhillwd@gmail.com>
2015-12-24 11:49:43 +0000

    destroy link and function working

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -19,11 +19,22 @@ class RecipesController < ApplicationController
   end
 
   def create
-      #binding.pry
-      @user = User.find_by(id: session[:user_id])
-      Recipe.create(recipe_params)
-      redirect_to(user_path(@user.id))
-    end
+    #binding.pry
+    @user = User.find_by(id: session[:user_id])
+    Recipe.create(recipe_params)
+    redirect_to(user_path(@user.id))
+  end
+
+  def destroy
+    @user = User.find_by(id: session[:user_id])
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+    redirect_to(user_path(@user.id))
+  end
+
+  def edit
+
+  end
 
     private
 
